208a579b010d58245e34fa2cb53c89ef2082fb46
Colton Malone <ctmalone15@gmail.com>
2014-11-07 18:53:44 -0700

    adds website input to contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,13 +6,4 @@ class ContactMailer < ActionMailer::Base
     mail(to: 'ctmalone15@gmail.com', subject: 'A Message from ColtonMalone.com')
    # here
   end
-
-  def send_simple_message
-	  RestClient.post "https://api:key-3ax6xnjp29jd6fds4gc373sgvjxteol0"\
-	  "@api.mailgun.net/v2/samples.mailgun.org/messages",
-	  :from => "Excited User <excited@samples.mailgun.org>",
-	  :to => "devs@mailgun.net",
-	  :subject => "Hello",
-	  :text => "Testing some Mailgun awesomeness!"
-	end
 end
